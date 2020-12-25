f7b2773caa0eff96658bce83d4148739709f9fd0
antisvin <antisvin@gmail.com>
2020-12-25 23:39:37 +0300

    Resource v3 version - hopefully the last one

--- Resources/ResSamplerPatch.hpp
+++ Resources/ResSamplerPatch.hpp
@@ -2,30 +2,21 @@
 #define __ResSamplerPatch_hpp__
 
 #include "Patch.h"
-#include "ResourceStorage.h"
+#include "Resource.h"
 #include "Envelope.h"
 #include "Oscillator.h"
 #include "VoltsPerOctave.h"
 #include "Control.h"
 #include "SmoothValue.h"
+#include "ScreenBuffer.h"
 
 #define SAMPLE_NAME "sample.wav"
 #define MIDDLE_C 261.6
 
 // Choose one of the 3 options above
 //#define RESOURCE_MEMORY_MAPPED
-//#define RESOURCE_DYNAMIC
-#define RESOURCE_STATIC
+#define RESOURCE_LOADED
 
-// Must be set for static resource test (size in bytes):
-#define RESOURCE_SIZE 51972
-
-#if defined(RESOURCE_STATIC) && defined(RESOURCE_SIZE)
-// Some wizardry for static allocation to SRAM
-static constexpr uint32_t resource_length = RESOURCE_SIZE / sizeof(float); // Elements count
-using ResourceBuffer = float[resource_length];
-using SampleResource = MemoryResource<ResourceBuffer>;
-#endif
 
 /*
   need a buffer that can be read in a circular fashion
@@ -33,13 +24,13 @@ using SampleResource = MemoryResource<ResourceBuffer>;
 class SampleOscillator : public Oscillator {
 private:
     const float fs;
-    FloatArray buffer;
+    const FloatArray buffer;
     float pos = 0.0f;
     uint16_t size;
     float rate = 1.0f;
 
 public:
-    SampleOscillator(float sr, FloatArray sample)
+    SampleOscillator(float sr, const FloatArray sample)
         : fs(sr)
         , buffer(sample) {
         size = buffer.getSize() - 1;
@@ -60,7 +51,7 @@ public:
         else if (pos > size)
             pos -= size;
     }
-    static float interpolate(float index, FloatArray data) {
+    static float interpolate(float index, const FloatArray& data) {
         int idx = (int)index;
         float low = data[idx];
         float high = data[idx + 1];
@@ -107,7 +98,7 @@ private:
     Control<PARAMETER_AF> duration = 1.0f;
 
 public:
-    SamplerVoice(float sr, FloatArray buf)
+    SamplerVoice(float sr, const FloatArray& buf)
         : osc(sr, buf)
         , env(sr)
         , gain(1.0f) {
@@ -115,7 +106,7 @@ public:
         env.setDecay(0.0);
         env.setRelease(0.0);
     }
-    static SamplerVoice* create(float sr, FloatArray buf) {
+    static SamplerVoice* create(float sr, const FloatArray& buf) {
         return new SamplerVoice(sr, buf);
     }
     static void destroy(SamplerVoice* voice) {
@@ -164,7 +155,7 @@ private:
     void take(uint8_t ch, uint8_t note, uint16_t velocity, uint16_t samples) {
         notes[ch] = note;
         allocation[ch] = TAKEN;
-        voice[ch]->setGain(velocity / (4095.0f * (VOICES / 2)));
+        voice[ch]->setGain(velocity / (4095.0f * (VOICES / 4)));
         voice[ch]->setGate(true, samples);
     }
 
@@ -176,7 +167,7 @@ private:
 
 public:
     Voices() = default;
-    Voices(float sr, int bs, FloatArray buf)
+    Voices(float sr, int bs, const FloatArray& buf)
         : allocated(0) {
         for (int i = 0; i < VOICES; ++i) {
             voice[i] = SamplerVoice::create(sr, buf);
@@ -190,6 +181,13 @@ public:
         for (int i = 0; i < VOICES; ++i)
             SamplerVoice::destroy(voice[i]);
         FloatArray::destroy(buffer);
+    }
+
+    int getAllocatedCount() {
+        int count = 0;
+        for (int i = 0; i < VOICES; ++i)
+            count += allocation[i] == TAKEN;
+        return count;
     }
 
     void allNotesOff() {
@@ -246,63 +244,103 @@ public:
     }
 };
 
-#ifdef RESOURCE_MEMORY_MAPPED
-Resource* resource;
-#elif defined(RESOUCE_DYNAMIC)
-DynamicMemoryResource resource;
-#elif defined(RESOURCE_STATIC)
-// This is a global object to make sure it's allocated on SRAM.
-SampleResource resource;
-#endif
-
-
 class ResSamplerPatch : public Patch {
 private:
     Voices<8> voices;
-    ResourceStorage storage;
     bool is_loaded;
 
+    #ifdef RESOURCE_MEMORY_MAPPED
+    const Resource* resource;
+    #else
+    Resource resource;
+    #endif
+
+#ifdef USE_SCREEN
+    static constexpr int8_t preview_scale = 50;
+    int8_t preview_hi[128];
+    int8_t preview_lo[128];
+
+    void storePreview(const FloatArray& sample){
+        // This ended up not particularly pretty, but at least gives a hint about loaded data
+        size_t length = sample.getSize();
+        size_t step = length / 128;
+        for (int i = 0; i < 128; i++) {
+            float max_val = 0, min_val = 0;
+            for (size_t j = i * step; j < (i + 1) * step; j++){
+                float val = sample[j];
+                max_val = max(val, max_val);
+                min_val = min(val, min_val);
+            }
+            preview_hi[i] = max_val * preview_scale;
+            preview_lo[i] = min_val * preview_scale;
+        }
+    }
+#endif
 public:
+#ifdef USE_SCREEN
+    void processScreen(ScreenBuffer& screen){
+#ifdef RESOURCE_MEMORY_MAPPED
+        screen.print(110, 10, "F");
+#elif defined(RESOURCE_LOADED)
+        screen.print(110, 10, "M");
+#endif
+        for (int i = 0; i < screen.getWidth(); i++) {
+            screen.setPixel(i, 20 + preview_hi[i], WHITE);
+            screen.setPixel(i, 20 + preview_lo[i], WHITE);
+        }
+        screen.setCursor(110, 44);
+        screen.print(voices.getAllocatedCount());
+        screen.print("v.");
+   }
+#endif
     ResSamplerPatch() : Patch() {
         registerParameter(PARAMETER_A, "Pitchbend");
         registerParameter(PARAMETER_D, "Envelope");
-        FloatArray sample;
+
+        registerParameter(PARAMETER_AA, "Env Attack");
+        registerParameter(PARAMETER_AB, "Env Decay");
+        registerParameter(PARAMETER_AC, "Env Sustain");
+        registerParameter(PARAMETER_AD, "Env Release");
+        //registerParameter(PARAMETER_AE, "Portamento");
+        registerParameter(PARAMETER_AF, "Duration");
 
         #ifdef RESOURCE_MEMORY_MAPPED
-        resource = storage.getResource(SAMPLE_NAME);
+        resource = Resource::get(SAMPLE_NAME);
         if (resource != NULL){
-            sample = resource->asArray<FloatArray, float>();
+            const FloatArray sample = resource->asArray<FloatArray, float>();
+            //FloatArray* sample_ptr = const_cast<FloatArray*>(&sample);
+            voices = Voices<8>(getSampleRate(), getBlockSize(), sample);
+#ifdef USE_SCREEN
+            storePreview(sample);
+#endif
+            voices = Voices<8>(getSampleRate(), getBlockSize(), sample);
             is_loaded = true;
         }
-        #elif defined(RESOURCE_DYNAMIC)
-        if (storage.loadResource(SAMPLE_NAME, resource)){
+        #elif defined(RESOURCE_LOADED)
+        FloatArray sample;
+        resource = Resource::load(SAMPLE_NAME);
+        if (resource.hasData()){
             sample = resource.asArray<FloatArray, float>();
+#ifdef USE_SCREEN
+            storePreview(sample);
+#endif
             is_loaded = true;
-        }
-        #elif defined(RESOURCE_STATIC)
-        if (storage.loadResource<ResourceBuffer>(SAMPLE_NAME, resource, 0)){
-            sample = resource.asArray<FloatArray, float>();
-            is_loaded = true;
-        }
-        #endif
-
-        if (is_loaded){
-            debugMessage("Sample loaded");
-            
-            #if defined(RESOURCE_DYNAMIC) || defined(RESOURCE_STATIC)
             // Sample is immutable if we read from flash
             float divisor = max(abs(sample.getMaxValue()), abs(sample.getMinValue()));
             if (divisor > 0.0f)
                 sample.multiply(1.0f / divisor);
-            #endif
-
             voices = Voices<8>(getSampleRate(), getBlockSize(), sample);
         }
-        else {
-            debugMessage("Sample NOT loaded");
-        }
+        #endif
+
     }
     ~ResSamplerPatch() {
+#ifdef RESOURCE_MEMORY_MAPPED
+        if (resource != NULL)
+            delete resource;
+#elif defined(RESOURCE_LOADED)
+        Resource::destroy(resource);
+#endif
     }
     
     void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples) {
@@ -310,7 +348,6 @@ public:
             return;
 
         if (bid == PUSHBUTTON) {
-            debugMessage("push", bid, value, samples);
             if (value)
                 voices.allNotesOn();
             else
